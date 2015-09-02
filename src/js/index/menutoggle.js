ad4167d8a590d6ac474bf4b2cbd1a79c8e28e417
Jascha Ehrenreich <jascha@jaeh.at>
2015-09-02 19:53:21 +0200

    made toggler addeventlistener safer

--- src/js/index/menutoggle.js
+++ src/js/index/menutoggle.js
@@ -3,21 +3,18 @@ import {hasClass, isFunction} from 'utils';
 const toggler = document.querySelector('nav.main .toggle');
 const toggleClassName = 'menu-toggled';
 
-console.log({toggler});
-
-toggler.addEventListener('click', () => {
-  document.body.classList.toggle(toggleClassName);
-});
+if (isFunction(toggler, 'addEventListener')) {
+  toggler.addEventListener('click', () => {
+    document.body.classList.toggle(toggleClassName);
+  });
+}
 
 const menuLinks = document.querySelectorAll('nav.main ul li a');
 
 Object.keys(menuLinks).forEach(key => {
   const link = menuLinks[key];
-  console.log('link', link);
   if (isFunction(link, 'addEventListener')) {
-    console.log('adding EventListener', link);
     link.addEventListener('click', () => {
-      console.log('click');
       if (hasClass(document.body, toggleClassName)) {
         document.body.classList.remove(toggleClassName);
       }
